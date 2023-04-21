9c81449670f6e85474523a07dc65bac7bcd5c2f0
furlat <tfurlanello@gmail.com>
2023-04-21 20:44:54 +0200

    Update memory_chat.py

--- babydragon/chat/memory_chat.py
+++ babydragon/chat/memory_chat.py
@@ -61,117 +61,49 @@ class VectorChat(VectorThread, BaseChat, Prompter):
         return answer
 
 
-
-class VectorPrompter(Prompter):
-    """This class handles prompting for the VectorChat, it overrides the default prompt_func to compose the prompt for the chat-gpt API."""
-    def __init__(self, system_prompt=None):
-        super().__init__(system_prompt=system_prompt)
-        self.prompt_func = self.vector_memory_prompt
-
-    def vector_memory_prompt(self, question, k=10):
-        prompt = [mark_system(self.system_prompt)]
-        prompt += self.get_token_bound_prompt(question, k=k)
-        prompt += [mark_question(self.user_prompt.format(question=question))]
-        return prompt, mark_question(self.user_prompt.format(question=question))
-
-        
-
-class VectorChat(VectorThread, BaseChat, Prompter):
-    """ Vector Memory combined with chat memory_prompt is constructed by filling the memory with the k most similar messages to the question till the max prompt memory tokens are reached"""
-    def __init__(self, index=None, name='vector_memory', max_context = 2048, system_prompt = None, user_prompt = None):
-        super().__init__(index, name, max_context)
-        if system_prompt is None:
-            self.system_prompt = self.get_default_system_prompt()
-        Chat.__init__(self, self.system_prompt, user_prompt)
-        self.prompt_func = self.vector_memory_prompt
-
-    def vector_memory_prompt(self, question, k = 10):
-        #starts by retieving the k most similar messages to the question
-        # then starting from the most similar message it adds the messages to the prompt till the max_prompt is reached
-        # the prompt is composed by the system prompt, the messages in the memory and the question
-        # the marked question is the last message in the prompt
-        
-        prompt = [mark_system(self.system_prompt)]
-        prompt +=  self.get_token_bound_prompt(question, k = k)
-        prompt+=[mark_question(self.user_prompt.format(question=question))]
-        
-        return prompt, mark_question(self.user_prompt.format(question=question))
-    
-    def query(self, question,verbose = False):
-        #compose the prompt for the chat-gpt api
-        prompt, marked_question = self.prompt_func(question)
-
-        if verbose:
-            print("prompt: ", prompt)
-        #call the chat-gpt api
-        response, success = self.chat_response(prompt)
-        if success:
-            #add the question and answer to the chat_history
-            answer = self.get_mark_from_response(response)
-            self.add_message(marked_question)
-            self.add_message(answer)
-            #get the answer from the open ai response
-            return answer
-        else:
-            return response.choices[0].message.content
-        
-class FifoVectorChat(FifoThread,Chat):
-    def __init__(self, system_prompt= None , name= 'fifo_vector_memory', max_memory = 2048, longterm_thread = None, longterm_frac = 0.5):
+class FifoVectorChat(FifoThread, BaseChat, Prompter):
+    def __init__(self, system_prompt=None, user_prompt=None, name='fifo_vector_memory', max_memory=2048, max_output_tokens= 1000, longterm_thread=None, longterm_frac=0.5):
         self.total_max_memory = max_memory
-        self.setup_longterm_memory(longterm_thread, max_memory , longterm_frac)
-        
-        super().__init__(name, self.max_short_term_memory, self.longterm_thread)
-        if system_prompt is None:
-            self.system_prompt = self.get_default_system_prompt()
-        Chat.__init__(self, self.system_prompt)
+
+        self.setup_longterm_memory(longterm_thread, max_memory, longterm_frac)
+        FifoThread.__init__(self, name=name, max_memory=self.max_short_term_memory, longterm_thread=self.longterm_thread)
+        BaseChat.__init__(self, max_output_tokens=max_output_tokens)
+        Prompter.__init__(self, system_prompt=system_prompt, user_prompt=user_prompt)
         self.prompt_func = self.fifovector_memory_prompt
         self.prompt_list = []
 
-    def setup_longterm_memory(self, longterm_thread, max_memory , longterm_frac):
+    def setup_longterm_memory(self, longterm_thread, max_memory, longterm_frac):
         if longterm_thread is None:
             self.longterm_frac = longterm_frac
-            self.max_short_term_memory =int(max_memory * (1-self.longterm_frac))
-            self.max_longterm_memory = max_memory - self.max_short_term_memory    
-            self.longterm_thread = VectorThread(None, 'longterm_memory',max_context = self.max_longterm_memory)
+            self.max_fifo_memory = int(max_memory * (1-self.longterm_frac))
+            self.max_vector_memory = max_memory - self.max_fifo_memory    
+            self.longterm_thread = VectorThread(None, 'longterm_memory', max_context=self.max_vector_memory)
         else:
             self.longterm_thread = longterm_thread
-            self.max_longterm_memory = self.longterm_thread.max_context
-            self.max_short_term_memory = self.total_max_memory - self.max_longterm_memory
-            self.longterm_frac = self.max_longterm_memory/self.total_max_memory
-    
-    def fifovector_memory_prompt(self, question, k = 10):
-        # compose the prompt for the chat-gpt api
-        # the first half of the prompt is composed by long term memory with up to max_longterm_memory tokens
-        # the second half ot the prompt is composed by the fifo memory with up to max_short_term_memory tokens
-        # the prompt is composed by the system prompt, the messages in the memory and the question
+            self.max_vector_memory = self.longterm_thread.max_context
+            self.max_fifo_memory = self.total_max_memory - self.max_longterm_memory
+            self.longterm_frac = self.max_vector_memory / self.total_max_memory
 
+    def fifovector_memory_prompt(self, question, k=10):
         prompt = [mark_system(self.system_prompt)]
-        #check if something is in the long term memory and if it is smaller than the max_longterm_memory
-        if len(self.longterm_thread.memory_thread) > 0 and self.longterm_thread.total_tokens <= self.max_longterm_memory:
-            #add all the messages in the long term memory
-            prompt+=self.longterm_thread.memory_thread
-        elif len(self.longterm_thread.memory_thread) > 0 and self.longterm_thread.total_tokens > self.max_longterm_memory:
-            # if the long term memory is bigger than the max_longterm_memory then add the k most similar messages to the question till the max_longterm_memory is reached
-            prompt += self.longterm_thread.get_token_bound_prompt(question, k =k)
-        
-        # add the complete short term memory to the prompt because it is a fifo memory is always smaller than the max_short_term_memory
-        prompt+=self.memory_thread
-        prompt+=[mark_question(self.user_prompt.format(question=question))]
+        if len(self.longterm_thread.memory_thread) > 0 and self.longterm_thread.total_tokens <= self.max_vector_memory:
+            prompt += self.longterm_thread.memory_thread
+        elif len(self.longterm_thread.memory_thread) > 0 and self.longterm_thread.total_tokens > self.max_vector_memory:
+            sorted_messages, sorted_scores, sorted_indices = self.longterm_thread.sorted_query(question, k=k, max_tokens = self.max_vector_memory,reverse_order=True) 
+            prompt += sorted_messages
+
+        prompt += self.memory_thread
+        prompt += [mark_question(self.user_prompt.format(question=question))]
         return prompt, mark_question(self.user_prompt.format(question=question))
 
-    def query(self, question):
-        #compose the prompt for the chat-gpt api
-        prompt, marked_question = self.prompt_func(question)
-        self.prompt_list.append(prompt)
-        #call the chat-gpt api
-        response, success = self.chat_response(prompt)
-        if success:
-            #add the question and answer to the chat_history
-            answer = self.get_mark_from_response(response)
-            self.add_message(marked_question)
-            self.add_message(answer)
-            #get the answer from the open ai response
-            return answer
-        else:
-            return response.choices[0].message.content
+    def query(self, question, verbose=False):
+        answer = BaseChat.query(self, message=question, verbose=verbose)
+        marked_question = mark_question(question)
+        self.add_message(marked_question)
+        self.add_message(answer)
+        return answer
     
+
+
+
+        
