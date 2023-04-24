3d7df90057750a67dce2777dbbfe6fd2f4581c90
furlat <tfurlanello@gmail.com>
2023-04-24 21:00:07 +0200

    added python_index

---
+++ babydragon/memory/indexes/python_index.py
@@ -0,0 +1,41 @@
+from typing import Optional
+from babydragon.memory.indexes.memory_index import MemoryIndex
+from babydragon.processors.parsers.python_parser import PythonParser
+import tiktoken
+
+class PythonIndex(MemoryIndex, PythonParser):
+    def __init__(
+        self,
+        directory_path: str,
+        name: str = "python_index",
+        save_path: Optional[str] = None,
+        load: bool = False,
+        minify_code: bool = False,
+        remove_docstrings: bool = False,
+        tokenizer: Optional[tiktoken.Encoding] = None,
+    ):
+        # Initialize the MemoryIndex
+        MemoryIndex.__init__(
+            self,
+            name=name,
+            save_path=save_path,
+            load=load,
+            tokenizer=tokenizer,
+        )
+        # Initialize the PythonParser
+        PythonParser.__init__(
+            self,
+            directory_path=directory_path,
+            minify_code=minify_code,
+            remove_docstrings=remove_docstrings,
+        )
+
+        if not load:
+            # Extract functions and classes source code
+            function_source_codes, class_source_codes, _, _ = self.process_directory()
+
+            # Concatenate function and class source code and index them
+            for code in function_source_codes + class_source_codes:
+                self.add_to_index(code)
+
+            self.save()
