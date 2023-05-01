d51e100964a38e1686affb4c1b0e48fea6a312a6
furlat <tfurlanello@gmail.com>
2023-05-01 23:27:54 +0200

    fixed rate limiter using decorator

--- babydragon/tasks/base_task.py
+++ babydragon/tasks/base_task.py
@@ -5,31 +5,44 @@ from concurrent.futures import ThreadPoolExecutor
 from typing import Any, List
 import json
 from babydragon.memory.indexes.memory_index import MemoryIndex
+import functools
 
 import time
 from threading import Lock
 
 class RateLimiter:
-    def __init__(self, calls_per_minute: int, max_workers: int = 1):
-        self.calls_per_minute = calls_per_minute // max_workers
+    def __init__(self, calls_per_minute: int):
+        self.calls_per_minute = calls_per_minute
         self.interval = 60 / calls_per_minute
         self.lock = Lock()
         self.last_call_time = None
 
-    def wait(self) -> None:
-        with self.lock:
-            if self.last_call_time is not None:
-                time_since_last_call = time.time() - self.last_call_time
-                if time_since_last_call < self.interval:
-                    time_to_wait = self.interval - time_since_last_call
-                    print(f"RateLimiter: Waiting for {time_to_wait:.2f} seconds before next call.")
-                    time.sleep(time_to_wait)
+    def __call__(self, func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            with self.lock:
+                if self.last_call_time is not None:
+                    time_since_last_call = time.time() - self.last_call_time
+                    if time_since_last_call < self.interval:
+                        time_to_wait = self.interval - time_since_last_call
+                        print(f"RateLimiter: Waiting for {time_to_wait:.2f} seconds before next call.")
+                        time.sleep(time_to_wait)
+                    else:
+                        print(f"RateLimiter: No wait required, time since last call: {time_since_last_call:.2f} seconds.")
                 else:
-                    print(f"RateLimiter: No wait required, time since last call: {time_since_last_call:.2f} seconds.")
-            else:
-                print("RateLimiter: This is the first call, no wait required.")
-            self.last_call_time = time.time()
+                    print("RateLimiter: This is the first call, no wait required.")
+                self.last_call_time = time.time()
+            return func(*args, **kwargs)
+        return wrapper
 
+class RateLimitedThreadPoolExecutor(ThreadPoolExecutor):
+    def __init__(self, max_workers=None, *args, **kwargs):
+        super().__init__(max_workers)
+        self.rate_limiter = RateLimiter(kwargs.get('calls_per_minute', 20))
+
+    def submit(self, fn, *args, **kwargs):
+        rate_limited_fn = self.rate_limiter(fn)
+        return super().submit(rate_limited_fn, *args, **kwargs)
 
 class BaseTask:
     def __init__(self, index: MemoryIndex, path: List[List[int]], max_workers: int = 1, task_id: str = "task", calls_per_minute: int = 20):
@@ -39,7 +52,7 @@ class BaseTask:
         self.results = []
         self.max_workers = max_workers
         self.parallel = True if max_workers > 1 else False
-        self.rate_limiter = RateLimiter(calls_per_minute, max_workers)
+        self.rate_limiter = RateLimiter(calls_per_minute)
 
 
     def _save_results_to_file(self) -> None:
@@ -64,22 +77,19 @@ class BaseTask:
     def execute_task(self) -> None:
         self._load_results_from_file()
 
-        with ThreadPoolExecutor(max_workers=self.max_workers) as executor:
+        with RateLimitedThreadPoolExecutor(max_workers=self.max_workers, calls_per_minute=self.rate_limiter.calls_per_minute) as executor:
             futures = []
             print(f"Executing task {self.task_id} using {self.max_workers} workers.")
 
             for i, sub_path in enumerate(self.path):
                 if i < len(self.results):
-                    # print(f"Sub-task {i} already completed, skipping...")
                     pass
                 else:
-                    # print(f"Submitting sub-task {i}...")
                     future = executor.submit(self._execute_sub_task, sub_path)
                     futures.append((i, future))
 
             for i, future in futures:
                 try:
-                    self.rate_limiter.wait()  # Add rate limiting here.
                     execution_start_time = time.time()
                     sub_task_result = future.result()
                     execution_end_time = time.time()
